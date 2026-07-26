3029942c9c6b0be9274c47d6096b00d84764bf99
Rashisky <rashisky007@gmail.com>
2026-07-26 21:06:04 +0100

    Request and Response structs for users

---
+++ internal/infrastructure/http/api/v1/user/user_request.go
@@ -0,0 +1,7 @@
+package user
+
+type UserCreateRequest struct {
+	FullName string `json:"full_name" binding:"required"`
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=3"`
+}
