d7ca78d9699d5ed7e9d0e23ca9e4619e0017c259
Rashisky <rashisky007@gmail.com>
2026-08-08 21:01:32 +0100

    Return slice of user values for FindAll. return user and error for Update

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -5,7 +5,7 @@ import "github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
 type UserRepository interface {
 	Create(user *entity.User) error
 	FindByID(userID entity.UserID) (*entity.User, error)
-	FindAll() ([]*entity.User, error)
+	FindAll() ([]entity.User, error)
 	DeleteByID(userID entity.UserID) error
-	Update(user *entity.User) error
+	Update(user *entity.User) (*entity.User, error)
 }
