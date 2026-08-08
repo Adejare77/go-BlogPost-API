d935be46ad5230db46220e5269a0ecb61ccef2e4
Rashisky <rashisky007@gmail.com>
2026-08-08 07:55:38 +0100

    Update LIkeId type

--- internal/domain/post/repository.go
+++ internal/domain/post/repository.go
@@ -6,9 +6,10 @@ import (
 
 type PostRepository interface {
 	Create(post *entity.Post) error
-	FindByID(postID entity.PostID) (*entity.Post, error)
-	Update(post *entity.Post) error
-	DeleteByID(postID entity.PostID) error
+	// FindByID(postID entity.PostID) (*entity.Post, error)
+	FindByID(postID entity.PostID, userID *entity.UserID) (*PostDetail, error)
+	Update(post *entity.Post) (*PostDetail, error)
+	DeleteByID(postID entity.PostID, userID entity.UserID) error
 	FindAll() ([]*PostList, error)
-	FindPostDetail(postID entity.PostID, userID entity.UserID) ([]*PostDetail, error)
+	// FindPostDetail(postID entity.PostID, userID *entity.UserID) ([]*PostDetail, error)
 }
