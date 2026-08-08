5f085bb34c949cde47df09ebb667a242a0716a32
Rashisky <rashisky007@gmail.com>
2026-08-08 21:22:45 +0100

    Return user entity for update.

---
+++ internal/usecase/post_service.go
@@ -0,0 +1,34 @@
+package usecase
+
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/post"
+)
+
+type PostService struct {
+	repo post.PostRepository
+}
+
+func (s *PostService) Create(post *entity.Post) error {
+	return s.repo.Create(post)
+}
+
+func (s *PostService) FindByID(postID entity.PostID, userID *entity.UserID) (*post.PostDetail, error) {
+	return s.repo.FindByID(postID, userID)
+}
+
+func (s *PostService) Update(post *entity.Post) (*post.PostDetail, error) {
+	return s.repo.Update(post)
+}
+
+func (s *PostService) DeleteByID(postID entity.PostID, userID entity.UserID) error {
+	return s.repo.DeleteByID(postID, userID)
+}
+
+func (s *PostService) FindAll() ([]post.PostList, error) {
+	return s.repo.FindAll()
+}
+
+// func (s *PostService) FindPostDetail(postID entity.PostID, userID entity.UserID) ([]*entity.PostDetail, error) {
+// 	return s.repo.FindPostDetail(postID, userID)
+// }
