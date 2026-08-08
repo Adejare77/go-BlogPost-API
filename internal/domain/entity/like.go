57bf8a985a9e309ea06129fe2410e652b36d9ed3
Rashisky <rashisky007@gmail.com>
2026-08-08 07:54:44 +0100

    LikeID typed to uuid.UUID

--- internal/domain/entity/like.go
+++ internal/domain/entity/like.go
@@ -1,10 +1,12 @@
 package entity
 
-type LikeID string
+import "github.com/google/uuid"
+
+type LikeID uuid.UUID
 
 type Like struct{
 	ID LikeID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	UserID UserID `gorm:"not null;uniqueIndex:idx_user_like"`
-	LikeableID string `gorm:"not null;uniqueIndex:idx_user_like"`
+	LikeableID LikeID `gorm:"not null;uniqueIndex:idx_user_like"`
 	LikeableType string `gorm:"not null;uniqueIndex:idx_user_like"`
 }
