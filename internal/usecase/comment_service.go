93ae3de7186f2288cebfa946adc3dbf7519022dd
Rashisky <rashisky007@gmail.com>
2026-08-14 22:03:54 +0100

    Comment Service

---
+++ internal/usecase/comment_service.go
@@ -0,0 +1,26 @@
+package usecase
+
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/comment"
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+)
+
+type CommentService struct {
+	repo comment.CommentRepository
+}
+
+func (s *CommentService) Create(comment *entity.Comment) error {
+	return s.repo.Create(comment)
+}
+
+func (s *CommentService) FindByID(commentID entity.CommentID, userID entity.UserID) (*comment.CommentDetail, error) {
+	return s.repo.FindByID(commentID, userID)
+}
+
+func (s *CommentService) Update(comment *entity.Comment) (*comment.CommentDetail, error) {
+	return s.repo.Update(comment)
+}
+
+func (s *CommentService) DeleteByID(commentID entity.CommentID, userID entity.UserID) error {
+	return s.repo.DeleteByID(commentID, userID)
+}
