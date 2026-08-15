9f4074eaf1ae28bb03d5dd7f5438392dedfcb931
Rashisky <rashisky007@gmail.com>
2026-08-15 22:28:10 +0100

    add find all posts with users liked. Update findbyId userid to value

--- internal/domain/post/repository.go
+++ internal/domain/post/repository.go
@@ -6,10 +6,8 @@ import (
 
 type PostRepository interface {
 	Create(post *entity.Post) error
-	// FindByID(postID entity.PostID) (*entity.Post, error)
-	FindByID(postID entity.PostID, userID *entity.UserID) (*PostDetail, error)
+	FindByID(postID entity.PostID, userID entity.UserID) (*PostDetail, error)
 	Update(post *entity.Post) (*PostDetail, error)
 	DeleteByID(postID entity.PostID, userID entity.UserID) error
-	FindAll() ([]*PostList, error)
-	// FindPostDetail(postID entity.PostID, userID *entity.UserID) ([]*PostDetail, error)
+	FindAll(userID entity.UserID) ([]PostList, error)
 }
