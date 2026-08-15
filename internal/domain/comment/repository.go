cfaa8429908a80d3f9a44ab93035aefca2f70e84
Rashisky <rashisky007@gmail.com>
2026-08-15 22:30:11 +0100

    add findbypostID and update findbyId user's id to value

--- internal/domain/comment/repository.go
+++ internal/domain/comment/repository.go
@@ -4,9 +4,9 @@ import "github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
 
 type CommentRepository interface {
 	Create(comment *entity.Comment) error
-	// FindByID(commentID entity.CommentID) (*entity.Comment, error)
-	FindByID(commentID entity.CommentID, userID *entity.UserID) (*CommentDetail, error)
+	FindByID(commentID entity.CommentID, userID entity.UserID) (*CommentDetail, error)
 	Update(comment *entity.Comment) (*CommentDetail, error)
 	DeleteByID(commentID entity.CommentID, userID entity.UserID) error
 	// FindByCommentID(commentID entity.CommentID) ([]*CommentDetail, error)
+	FindByPostID(postID entity.PostID, userID entity.UserID) ([]CommentList, error)
 }
