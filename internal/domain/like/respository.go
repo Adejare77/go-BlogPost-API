c84e411bbecf3b33b433fd117cf6454224698206
Rashisky <rashisky007@gmail.com>
2026-08-16 21:11:56 +0100

    remove exists and count methods

--- internal/domain/like/respository.go
+++ internal/domain/like/respository.go
@@ -4,10 +4,6 @@ import "github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
 
 type LikeRepository interface {
 	Create(like *entity.Like) error
-	ExistByUserAndPost(userID entity.UserID, postID entity.LikeID) (bool, error)
-	ExistByUserAndComment(userID entity.UserID, commentID entity.LikeID) (bool, error)
 	DeleteByUserAndPost(userID entity.UserID, postID entity.LikeID) error
 	DeleteByUserAndComment(userID entity.UserID, commentID entity.LikeID) error
-	CountByPost(postID entity.LikeID) (int, error)
-	CountByComment(commentID entity.LikeID) (int, error)
 }
