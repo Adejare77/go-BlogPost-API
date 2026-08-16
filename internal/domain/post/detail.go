247bff05cd069f6a407f6d93724ae454ca121fbd
Rashisky <rashisky007@gmail.com>
2026-08-16 21:09:07 +0100

    remove commentsummary struct

--- internal/domain/post/detail.go
+++ internal/domain/post/detail.go
@@ -3,23 +3,13 @@ package post
 import (
 	"time"
 
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/comment"
 	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
 )
 
 type AuthorSummary struct {
 	ID entity.UserID
 	FullName string
-}
-
-type CommentSummary struct {
-	ID entity.CommentID
-	Author AuthorSummary
-	PostID entity.PostID
-	Excerpt string
-	Likes int
-	Liked bool
-	ReplyCount int
-	CreatedAt time.Time
 }
 
 type PostDetail struct {
@@ -31,6 +21,6 @@ type PostDetail struct {
 	Liked bool
 	IsPublished bool
 	CommentCount int
-	TopComments []CommentSummary
+	TopComments []comment.CommentList
 	CreatedAt time.Time
 }
