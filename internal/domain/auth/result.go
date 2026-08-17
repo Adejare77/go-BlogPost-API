a47a8c2b7b289277a39ddd7f30d2e3c800fac171
Rashisky <rashisky007@gmail.com>
2026-08-17 22:51:39 +0100

    add authentication token and result

---
+++ internal/domain/auth/result.go
@@ -0,0 +1,10 @@
+package auth
+
+import "github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+
+type AuthResult struct {
+	AccessToken string
+	RefreshToken string
+	UserID entity.UserID
+	Email string
+}
---
+++ internal/domain/auth/token.go
@@ -0,0 +1,8 @@
+package auth
+
+import "github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+
+type TokenService interface {
+	GenerateAccessToken(userID entity.UserID) (string, error)
+	GenerateRefreshToken(userID entity.UserID) (string, error)
+}
