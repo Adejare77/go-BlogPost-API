108b8d878f614ffd2bbcc0b49113442565437c8e
Rashisky <rashisky007@gmail.com>
2026-08-21 21:08:19 +0100

    update auth response and add authtokenresponse struct

--- internal/delivery/http/handler/v1/auth/response.go
+++ internal/delivery/http/handler/v1/auth/response.go
@@ -1,11 +1,20 @@
 package auth
 
 import (
+	"time"
+
 	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
 )
 
-type AuthResponse struct {
+type AuthTokenResponse struct {
 	AccessToken string `json:"access_token"`
 	UserID entity.UserID `json:"user_id"`
 	Email string `json:"email"`
+}
+
+type AuthResponse struct {
+	ID entity.UserID `json:"id"`
+	FullName string `json:"full_name"`
+	Email string `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
