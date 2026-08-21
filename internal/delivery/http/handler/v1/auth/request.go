fbbe46da1dde46c5ce03151f194a02f7229be432
Rashisky <rashisky007@gmail.com>
2026-08-21 21:06:41 +0100

    add authregister struct

--- internal/delivery/http/handler/v1/auth/request.go
+++ internal/delivery/http/handler/v1/auth/request.go
@@ -4,3 +4,9 @@ type AuthRequest struct {
 	Email string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=3"`
 }
+
+type AuthRegister struct {
+	FullName string `json:"full_name" binding:"required"`
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=3"`
+}
