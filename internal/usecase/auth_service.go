cb0f46ad1034c4b726c1a0ba591118ef97c50a44
Rashisky <rashisky007@gmail.com>
2026-08-21 21:04:23 +0100

    add refresh token, findbyId, logout etc methods

--- internal/usecase/auth_service.go
+++ internal/usecase/auth_service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Adejare77/go-BlogPost-API/internal/domain/auth"
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
 	"github.com/Adejare77/go-BlogPost-API/internal/domain/user"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -48,4 +49,24 @@ func (s *AuthService) Login(email, password string) (*auth.AuthResult, error) {
 		UserID: user.ID,
 		Email: user.Email,
 	}, nil
+}
+
+func (s *AuthService) Logout(userID entity.UserID) error {}
+
+func (s *AuthService) Register(user *entity.User) error {
+	if user.Password != nil {
+		hash, err := bcrypt.GenerateFromPassword([]byte(*user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return fmt.Errorf("hash password: %w", err)
+		}
+		password := string(hash)
+		user.Password = &password
+	}
+
+	// log user created
+	return s.repo.Create(user)
+}
+
+func (s *AuthService) FindByID(userID entity.UserID) (*user.UserDetail, error) {
+	return s.repo.FindByID(userID)
 }
