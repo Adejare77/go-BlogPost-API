913e65f4230e0078491675a0ba6e036a9850e0d1
Rashisky <rashisky007@gmail.com>
2026-08-22 22:10:27 +0100

    update refresh token method

--- internal/usecase/auth_service.go
+++ internal/usecase/auth_service.go
@@ -10,35 +10,45 @@ import (
 )
 
 type AuthService struct {
-	repo user.UserRepository
-	token auth.TokenService
+	userRepo user.UserRepository
+	refreshTokenRepo auth.RefreshTokenRepository
+	tokenService auth.TokenService
 }
 
-func NewAuthService(repo user.UserRepository, token auth.TokenService) *AuthService {
+func NewAuthService(userRepo user.UserRepository, tokenService auth.TokenService) *AuthService {
 	return &AuthService{
-		repo: repo,
-		token: token,
+		userRepo: userRepo,
+		tokenService: tokenService,
 	}
 }
 
-func (s *AuthService) Login(email, password string) (*auth.AuthResult, error) {
-	user, err := s.repo.FindByEmail(email)
+func hashCredential(value string, cost int) (string, error) {
+	result, err := bcrypt.GenerateFromPassword([]byte(value), cost)
 	if err != nil {
-		return nil, fmt.Errorf("Invalid Credential")
+		return "", err
+	}
+
+	return string(result), nil
+}
+
+func (s *AuthService) Login(email, password string) (*auth.AuthResult, error) {
+	user, err := s.userRepo.FindByEmail(email)
+	if err != nil {
+		return nil, fmt.Errorf("invalid Credential")
 	}
 
 	if err := bcrypt.CompareHashAndPassword(
 		[]byte(*user.Password),
 		[]byte(password)); err != nil {
-			return nil, fmt.Errorf("Invalid Credential")
+			return nil, fmt.Errorf("invalid Credential")
 		}
 
-	accessToken, err := s.token.GenerateAccessToken(user.ID)
+	accessToken, err := s.tokenService.GenerateAccessToken(user.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	refreshToken, err := s.token.GenerateRefreshToken()
+	refreshToken, err := s.tokenService.GenerateRefreshToken()
 	if err != nil {
 		return nil, err
 	}
@@ -47,7 +57,6 @@ func (s *AuthService) Login(email, password string) (*auth.AuthResult, error) {
 		AccessToken: accessToken,
 		RefreshToken: refreshToken,
 		UserID: user.ID,
-		Email: user.Email,
 	}, nil
 }
 
@@ -55,18 +64,64 @@ func (s *AuthService) Logout(userID entity.UserID) error {}
 
 func (s *AuthService) Register(user *entity.User) error {
 	if user.Password != nil {
-		hash, err := bcrypt.GenerateFromPassword([]byte(*user.Password), bcrypt.DefaultCost)
+		password, err := hashCredential(*user.Password, bcrypt.DefaultCost)
 		if err != nil {
 			return fmt.Errorf("hash password: %w", err)
 		}
-		password := string(hash)
 		user.Password = &password
 	}
 
 	// log user created
-	return s.repo.Create(user)
+	return s.userRepo.Create(user)
 }
 
 func (s *AuthService) FindByID(userID entity.UserID) (*user.UserDetail, error) {
-	return s.repo.FindByID(userID)
+	return s.userRepo.FindByID(userID)
+}
+
+func (s *AuthService) RefreshToken(token string) (auth.AuthResult, error) {
+	tokenHash, err := hashCredential(token, bcrypt.MinCost)
+	if err != nil {
+		return auth.AuthResult{}, fmt.Errorf("error hashing token: %w", err)
+	}
+
+	retrieveToken, err := s.refreshTokenRepo.FindByTokenHash(tokenHash)
+	if err != nil {
+		return auth.AuthResult{}, fmt.Errorf("invalid token: %w", err)
+	}
+
+	if err := s.refreshTokenRepo.RevokeToken(retrieveToken.TokenHash); err != nil {
+		return auth.AuthResult{}, fmt.Errorf("error revoking token: %w", err)
+	}
+
+	// generate new refresh tokne
+	newRefreshToken, err := s.tokenService.GenerateRefreshToken()
+	if err != nil {
+		return auth.AuthResult{}, fmt.Errorf("error generating refresh token: %w", err)
+	}
+
+	// hash the new token
+	newRefreshTokenHash, err := hashCredential(newRefreshToken, bcrypt.MinCost)
+	if err != nil {
+		return auth.AuthResult{}, fmt.Errorf("error hasing token: %w", err)
+	}
+
+	refreshToken := entity.RefreshToken{
+		UserID: retrieveToken.UserID,
+		TokenHash: newRefreshTokenHash,
+	}
+	if err := s.refreshTokenRepo.Create(&refreshToken); err != nil {
+		return auth.AuthResult{}, fmt.Errorf("error creating new token in DB: %w", err)
+	}
+
+	accessToken, err := s.tokenService.GenerateAccessToken(retrieveToken.UserID)
+	if err != nil {
+		return auth.AuthResult{}, fmt.Errorf("error generating access token: %w", err)
+	}
+
+	return auth.AuthResult{
+		AccessToken: accessToken,
+		RefreshToken: newRefreshToken,
+		UserID: retrieveToken.UserID,
+	}, nil
 }
