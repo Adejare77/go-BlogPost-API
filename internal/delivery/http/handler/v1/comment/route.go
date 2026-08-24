292dbb80007556124f1449fd5ed0816b42901347
Rashisky <rashisky007@gmail.com>
2026-08-24 22:06:34 +0100

    udpate comment routes

--- internal/delivery/http/handler/v1/comment/route.go
+++ internal/delivery/http/handler/v1/comment/route.go
@@ -1,14 +1,31 @@
 package comment
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/delivery/middleware"
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/auth"
+	"github.com/gin-gonic/gin"
+)
 
-var CommentProtectedRoute = func(r *gin.RouterGroup, h *CommentHandler) {
-	r.POST("", h.CreateComment)
-	r.POST("", h.CreateReply)
-	r.PATCH("", h.Update)
-	r.DELETE("", h.DeleteByID)
+var commentProtectedRoute = func(r *gin.RouterGroup, h *CommentHandler) {
+	r.POST("/posts/:post_id/comments", h.CreateComment)
+	r.POST("/comments/:comment_id/replies", h.CreateReply)
+	r.PATCH("/comments/:comment_id", h.Update)
+	r.DELETE("/comments/:comment_id", h.DeleteByID)
 }
 
-var CommentPublicRoute = func(r *gin.RouterGroup, h *CommentHandler) {
-	r.GET("", h.FindByID)
+var commentPublicRoute = func(r *gin.RouterGroup, h *CommentHandler) {
+	r.GET("/comments", h.FindByID)
+}
+
+
+func RegisterRoutes(
+	r *gin.RouterGroup,
+	h *CommentHandler,
+	tokenService auth.TokenService,
+	) {
+		commentPublicRoute(r, h)
+
+		protected := r.Group("")
+		protected.Use(middleware.AuthMiddleware(tokenService))
+		commentProtectedRoute(protected, h)
 }
