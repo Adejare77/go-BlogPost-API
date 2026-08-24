b82030c9b352054b4754005be2ff84787047e71d
Rashisky <rashisky007@gmail.com>
2026-08-24 22:05:38 +0100

    add middleare authentication

---
+++ internal/delivery/middleware/auth.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/auth"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(tokenService auth.TokenService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("Authorization")
+		if token == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "unauthorized",
+			})
+		}
+
+		claimsAny, err := tokenService.Validate(token)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": err,
+			})
+		}
+		claims := claimsAny.(*Claims)
+
+		ctx.Set("userID", claims.UserID)
+
+		ctx.Next()
+	}
+}
