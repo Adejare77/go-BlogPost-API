32669bc449bfeb83edebfe3ef481d042ed026932
Rashisky <rashisky007@gmail.com>
2026-08-24 22:06:54 +0100

    update user routes

--- internal/delivery/http/handler/v1/user/route.go
+++ internal/delivery/http/handler/v1/user/route.go
@@ -1,8 +1,26 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/delivery/middleware"
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/auth"
+	"github.com/gin-gonic/gin"
+)
 
-var UserRoute = func(r *gin.RouterGroup, h *UserHandler) {
-	r.DELETE("", h.DeleteByID)
-	r.PATCH("", h.Update)
+var userRoute = func(r *gin.RouterGroup, h *UserHandler) {
+	r.GET("", h.FindAll)
+	r.GET("/:user_id", h.FindByID)
+	r.DELETE("/:user_id", h.DeleteByID)
+	r.PATCH("/:user_id", h.Update)
+}
+
+
+func RegisterRoutes(
+	r *gin.RouterGroup,
+	h *UserHandler,
+	tokenService auth.TokenService,
+	) {
+	protected := r.Group("/users")
+	protected.Use(middleware.AuthMiddleware(tokenService))
+
+	userRoute(protected, h)
 }
