9aec4f29aa9f031981648f3384e4278772f9f9f9
Rashisky <rashisky007@gmail.com>
2026-08-24 22:08:04 +0100

    add validated method to the interface

--- internal/domain/auth/token.go
+++ internal/domain/auth/token.go
@@ -1,8 +1,11 @@
 package auth
 
-import "github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+)
 
 type TokenService interface {
 	GenerateAccessToken(userID entity.UserID) (string, error)
 	GenerateRefreshToken() (string, error)
+	Validate(tokenString string) (any, error)
 }
