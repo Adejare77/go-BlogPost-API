04db92e6396fb9820ed06511ceb1198bb2a6d8d4
Rashisky <rashisky007@gmail.com>
2026-08-24 22:09:34 +0100

    update post routes

--- internal/delivery/http/handler/v1/post/route.go
+++ internal/delivery/http/handler/v1/post/route.go
@@ -1,14 +1,34 @@
 package post
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/delivery/middleware"
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/auth"
+	"github.com/gin-gonic/gin"
+)
 
-var PostProtectedRoute = func(r *gin.RouterGroup, h *PostHandler) {
-	r.POST("", h.Create)
-	r.DELETE("", h.DeleteByID)
-	r.PATCH("", h.Update)
+var postProtectedRoute = func(r *gin.RouterGroup, h *PostHandler) {
+	r.POST("/posts", h.Create)
+	r.DELETE("/posts/:post_id", h.DeleteByID)
+	r.PATCH("/Posts/:post_id", h.Update)
 }
 
-var PostPublicRoute = func(r *gin.RouterGroup, h *PostHandler) {
-	r.GET("", h.FindAll)
-	r.GET("", h.FindByID)
+var postPublicRoute = func(r *gin.RouterGroup, h *PostHandler) {
+	r.GET("/posts", h.FindAll)
+	r.GET("/posts/:post_id", h.FindByID)
+}
+
+
+func RegisterRoutes(
+	r *gin.RouterGroup,
+	h *PostHandler,
+	tokenService auth.TokenService,
+	) {
+	posts := r.Group("/posts")
+
+	postPublicRoute(posts, h)
+
+	protected := posts.Group("")
+	protected.Use(middleware.AuthMiddleware(tokenService))
+
+	postProtectedRoute(protected, h)
 }
