03064f108c1ad5290d236dda24784e11a079bb43
Rashisky <rashisky007@gmail.com>
2026-08-24 22:04:52 +0100

    add user authentication route

---
+++ internal/delivery/http/handler/v1/auth/route.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/delivery/middleware"
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/auth"
+	"github.com/gin-gonic/gin"
+)
+
+var authPublicRoute = func(r *gin.RouterGroup, h *AuthHandler) {
+	r.POST("/login", h.Login)
+	r.POST("/create", h.Create)
+	r.POST("/logout", h.Logout)
+}
+
+var authPrivateRoute = func(r *gin.RouterGroup, h *AuthHandler) {
+	r.GET("/me", h.Me)
+}
+
+func RegisterRoutes(
+	r *gin.RouterGroup,
+	h *AuthHandler,
+	tokenService auth.TokenService,
+) {
+	auth := r.Group("/auth")
+
+	authPublicRoute(auth, h)
+
+	protected := auth.Group("")
+	protected.Use(middleware.AuthMiddleware(tokenService))
+	authPrivateRoute(protected, h)
+}
