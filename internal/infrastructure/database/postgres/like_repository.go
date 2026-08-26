7e62eb519f5498c462ba7054b10f7c7ae4b3134c
Rashisky <rashisky007@gmail.com>
2026-08-26 19:40:32 +0100

    Use MapError query for errors

--- internal/infrastructure/database/postgres/like_repository.go
+++ internal/infrastructure/database/postgres/like_repository.go
@@ -16,17 +16,17 @@ func NewLikeRepository(db *gorm.DB) *LikeRepository {
 }
 
 func (repo *LikeRepository) Create(like *entity.Like) error {
-	return repo.db.Create(like).Error
+	return MapError(repo.db.Create(like).Error)
 }
 
 func (repo *LikeRepository) DeleteByUserAndPost(userID entity.UserID, postID entity.LikeID) error {
-	return repo.db.
+	return MapError(repo.db.
 	Where("id = ? AND likeable_type = 'post' AND author_id = ?", postID, userID).
-	Delete(&entity.Like{}).Error
+	Delete(&entity.Like{}).Error)
 }
 
 func (repo *LikeRepository) DeleteByUserAndComment(userID entity.UserID, commentID entity.LikeID) error {
-	return repo.db.
+	return MapError(repo.db.
 	Where("id = ? AND likeable_type = 'comment' AND author_id = ?", commentID, userID).
-	Delete(&entity.Like{}).Error
+	Delete(&entity.Like{}).Error)
 }
