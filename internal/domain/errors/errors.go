26c51348c5b59888aef6ada5af3abdf646089e44
Rashisky <rashisky007@gmail.com>
2026-08-27 20:16:04 +0100

    add application errors

---
+++ internal/domain/errors/errors.go
@@ -0,0 +1,10 @@
+package errors
+
+import "errors"
+
+var (
+	ErrNotFound = errors.New("resource not found")
+	ErrAlreadyExists = errors.New("already exists")
+	ErrTokenRevoked = errors.New("token already revoked")
+	ErrInvalidToken = errors.New("invalid or expired token")
+)
