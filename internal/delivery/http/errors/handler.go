df781d4ca631f7b646da15656d09d1737461094c
Rashisky <rashisky007@gmail.com>
2026-08-28 21:27:19 +0100

    add more domain/app errors. update HandleRequestError

--- internal/delivery/http/errors/handler.go
+++ internal/delivery/http/errors/handler.go
@@ -2,7 +2,6 @@ package errors
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
 
 	domainErrors "github.com/Adejare77/go-BlogPost-API/internal/domain/errors"
@@ -25,13 +24,23 @@ func HandleError(ctx *gin.Context, err error) {
 		code = "already_exists"
 		message = "email already exists"
 
+	case errors.Is(err, domainErrors.ErrInvalidCredentials) || errors.Is(err, domainErrors.ErrAccountDisabled):
+		status = http.StatusUnauthorized
+		code = "invalid_credentials"
+		message = "invalid credentials"
+
+	case errors.Is(err, domainErrors.ErrTokenRevoked) || errors.Is(err, domainErrors.ErrInvalidToken):
+		status = http.StatusUnauthorized
+		code = "invalid_or_expired_token"
+		message = "invalid or expired token"
+
 	default:
 		status = http.StatusInternalServerError
 		code = "internal_error"
 		message = "internal server error"
 	}
 
-	response := ErrorResponse{
+	response := errorResponse{
 		Code: code,
 		Message: message,
 	}
@@ -40,13 +49,9 @@ func HandleError(ctx *gin.Context, err error) {
 }
 
 
-func HandleRequestError(ctx *gin.Context, err error) {
-	status := http.StatusBadRequest
-	code := "bad_request"
-	message := fmt.Sprintf("Bad Request: %s", err.Error())
-
-	response := ErrorResponse{
-		Code: code,
+func HandleRequestError(ctx *gin.Context, status int, field, message string, err error) {
+	response := errorResponse{
+		Code: field,
 		Message: message,
 	}
 
