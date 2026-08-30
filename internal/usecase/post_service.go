f49bdb215d53151877787fdc891d4e6c76bd1fa7
Rashisky <rashisky007@gmail.com>
2026-08-30 18:30:25 +0100

    add query and isStaff parameter to FindAll, also isStaff to FindByID

--- internal/usecase/post_service.go
+++ internal/usecase/post_service.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+	domainErrors "github.com/Adejare77/go-BlogPost-API/internal/domain/errors"
 	"github.com/Adejare77/go-BlogPost-API/internal/domain/post"
 )
 
@@ -19,8 +20,17 @@ func (s *PostService) Create(post *entity.Post) error {
 	return s.repo.Create(post)
 }
 
-func (s *PostService) FindByID(postID entity.PostID, userID entity.UserID) (*post.PostDetail, error) {
-	return s.repo.FindByID(postID, userID)
+func (s *PostService) FindByID(postID entity.PostID, userID entity.UserID, isStaff bool) (*post.PostDetail, error) {
+	post, err := s.repo.FindByID(postID, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	if post.IsPublished || post.Author.ID == userID || isStaff {
+		return post, err
+	}
+
+	return nil, domainErrors.ErrNotFound
 }
 
 func (s *PostService) Update(post *entity.Post) (*post.PostDetail, error) {
@@ -31,6 +41,14 @@ func (s *PostService) DeleteByID(postID entity.PostID, userID entity.UserID) err
 	return s.repo.DeleteByID(postID, userID)
 }
 
-func (s *PostService) FindAll(userID entity.UserID) ([]post.PostList, error) {
-	return s.repo.FindAll(userID)
+func (s *PostService) FindAll(userID entity.UserID, query post.PostQuery, isStaff bool) ([]post.PostList, error) {
+	if userID == 0 && (query.Status == "draft" || query.Status == "all") {
+		return []post.PostList{}, domainErrors.ErrUnauthorized
+	}
+
+	if query.Status != "published" && query.Author != "me" && query.Author == "" && !isStaff {
+		return []post.PostList{}, domainErrors.ErrForbidden
+	}
+
+	return s.repo.FindAll(userID, query)
 }
