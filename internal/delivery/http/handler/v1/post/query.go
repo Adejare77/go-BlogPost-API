29e8826680f22eb1930f4702748b0cb96d7643d6
Rashisky <rashisky007@gmail.com>
2026-08-31 15:47:52 +0100

    omit fields when not given

--- internal/delivery/http/handler/v1/post/query.go
+++ internal/delivery/http/handler/v1/post/query.go
@@ -1,6 +1,6 @@
 package post
 
 type PostQueryRequest struct {
-	Status string `form:"status" binding:"oneof=all draft published"`
-	Author string `form:"author"`
+	Status string `form:"status" binding:"omitempty,oneof=all draft published"`
+	Author string `form:"author,omitempty"`
 }
