a81c65d745de2866f090752f9fc42cfeaed1a7f0
Rashisky <rashisky007@gmail.com>
2026-09-01 21:22:26 +0100

    revoke and find token by ID

--- internal/domain/auth/repository.go
+++ internal/domain/auth/repository.go
@@ -1,9 +1,12 @@
 package auth
 
-import "github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+import (
+	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
+	"github.com/google/uuid"
+)
 
 type RefreshTokenRepository interface {
 	Create(*entity.RefreshToken) error
-	RevokeToken(token string) error
-	FindByTokenHash(tokenHash string) (*entity.RefreshToken, error)
+	RevokeToken(tokenID uuid.UUID) error
+	FindByTokenID(tokenID uuid.UUID) (*entity.RefreshToken, error)
 }
