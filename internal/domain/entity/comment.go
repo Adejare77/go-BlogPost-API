7f458a5eb5dd31f06676ba053ccc6f0d3e237923
Rashisky <rashisky007@gmail.com>
2026-09-02 07:15:42 +0100

    add Scan, Value and MarshalJSON on CommentID

--- internal/domain/entity/comment.go
+++ internal/domain/entity/comment.go
@@ -1,6 +1,8 @@
 package entity
 
 import (
+	"database/sql/driver"
+	"encoding/json"
 	"time"
 
 	"github.com/google/uuid"
@@ -19,4 +21,25 @@ type Comment struct {
 	Replies []Comment `gorm:"foreignKey:ParentID;constraint:OnDelete:CASCADE"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+
+func (id *CommentID) Scan(value any) error {
+	var u uuid.UUID
+
+	if err := u.Scan(value); err != nil {
+		return err
+	}
+
+	*id = CommentID(u)
+
+	return nil
+}
+
+func (id CommentID) Value() (driver.Value, error) {
+	return uuid.UUID(id).Value()
+}
+
+func (id CommentID) MarshalJSON() ([]byte, error) {
+	return json.Marshal(uuid.UUID(id).String())
 }
