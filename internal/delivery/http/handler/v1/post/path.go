05c9b58c8a821a58072def86472d7a403a75a1bc
Rashisky <rashisky007@gmail.com>
2026-09-07 15:10:52 +0100

    string type field

--- internal/delivery/http/handler/v1/post/path.go
+++ internal/delivery/http/handler/v1/post/path.go
@@ -1,7 +1,5 @@
 package post
 
-import "github.com/google/uuid"
-
 type PostPathRequest struct {
-	PostID uuid.UUID `uri:"post_id" binding:"required,uuid"`
+	PostID string `uri:"post_id" binding:"required,uuid"`
 }
