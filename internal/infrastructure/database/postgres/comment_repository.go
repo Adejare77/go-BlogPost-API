cd3a872eb46db759aa1e9a74be1f8cfbb0f06f5a
Rashisky <rashisky007@gmail.com>
2026-09-07 14:52:17 +0100

    update repo to return flat struct, and update syntax errors

--- internal/infrastructure/database/postgres/comment_repository.go
+++ internal/infrastructure/database/postgres/comment_repository.go
@@ -18,26 +18,34 @@ func NewCommentRepository(db *gorm.DB) *CommentRepository {
 }
 
 func (repo *CommentRepository) Create(comment *entity.Comment) error {
-	return MapError(repo.db.Create(comment).Error)
+	if err := repo.db.Create(comment).Error; err != nil {
+		return MapError(repo.db.Create(comment).Error)
+	}
+
+	return MapError(
+		repo.db.Preload("Author").
+		First(comment, "id = ?", comment.ID).Error,
+	)
 }
 
-func (repo *CommentRepository) FindByID(commentID entity.CommentID, userID entity.UserID) (*comment.CommentDetail, error) {
-	var commentDetail CommentDetailRow
+func (repo *CommentRepository) FindByID(commentID entity.CommentID, userID entity.UserID) (*comment.CommentDetailRow, error) {
+	var commentDetail comment.CommentDetailRow
 
 	if err := repo.db.Model(&entity.Comment{}).
 	Select(`
 		comments.id AS id,
-		comments.author_id AS author_id
-		users.full_name AS full_name
-		comments.content AS content
-		comments.created_at AS
+		comments.author_id AS author_id,
+		users.full_name AS full_name,
+		comments.post_id AS post_id,
+		comments.content AS content,
+		comments.created_at AS created_at,
 
 		(
 			SELECT COUNT(*)
 			FROM likes
 			WHERE likes.likeable_id = comments.id
 			AND likes.likeable_type = 'comment'
-		) AS likes
+		) AS likes,
 
 		EXISTS (
 			SELECT 1
@@ -45,14 +53,14 @@ func (repo *CommentRepository) FindByID(commentID entity.CommentID, userID entit
 			WHERE likes.likeable_id = comments.id
 			AND likes.likeable_type = 'comment'
 			AND likes.user_id = ?
-		) AS liked
+		) AS liked,
 
 		(
 			SELECT COUNT(*)
 			FROM comments
 			WHERE comments.parent_id IS NOT NULL
 			AND comments.parent_id = comments.id
-		)
+		) AS reply_count
 	`, userID).
 	Joins("JOIN users ON users.id = comments.author_id").
 	Joins("JOIN posts ON posts.id = comments.post_id").
@@ -61,7 +69,7 @@ func (repo *CommentRepository) FindByID(commentID entity.CommentID, userID entit
 	}
 
 
-	var topRepliesRow []ReplySummaryRow
+	var topRepliesRow []comment.CommentListRow
 
 	if err := repo.db.Model(&entity.Comment{}).
 	Select(`
@@ -70,13 +78,8 @@ func (repo *CommentRepository) FindByID(commentID entity.CommentID, userID entit
 		users.full_name AS full_name,
 		posts.id AS post_id,
 		comments.content AS content,
-
-		(
-			SELECT COUNT(*)
-			FROM likes
-			WHERE likes.likeable_id = comments.id
-			AND likes.likeable_type = 'comment'
-		) AS likes
+		comments.created_at AS created_at,
+		comments.parent_id AS parent_id,
 
 		(
 			SELECT 1
@@ -84,36 +87,35 @@ func (repo *CommentRepository) FindByID(commentID entity.CommentID, userID entit
 			WHERE likes.likeable_type = 'comment'
 			AND likes.user_id = ?
 			AND likes.likeable_id = comments.id
-		) AS liked
+		) AS liked,
+
+		(
+			SELECT COUNT(*)
+			FROM likes
+			WHERE likes.likeable_id = comments.id
+			AND likes.likeable_type = 'comment'
+		) AS likes,
 
 		(
 			SELECT COUNT(*)
 			FROM comments
 			WHERE parent_id = ?
-		) AS comment_count
+		) AS reply_count
 
 	`, userID, commentID).
 	Joins("JOIN users ON users.id = comments.author_id").
 	Joins("JOIN posts ON posts.id = comments.post_id").
 	Where("parent_id = ?", commentID).
-	Order("likes DESC created_at DESC").
+	Order("likes DESC, created_at DESC").
 	Limit(3).
 	Scan(&topRepliesRow).Error; err != nil {
 		return nil, MapError(err)
 	}
 
-	topReplies := make([]comment.ReplySummary, len(topRepliesRow))
-
-	for i, reply := range topRepliesRow {
-		topReplies[i] = reply.ToReplySummary()
-	}
-
-	commentDetail.TopReplies = topReplies
-
-	return commentDetail.ToCommentDetail(), nil
+	return &commentDetail, nil
 }
 
-func (repo *CommentRepository) Update(comment *entity.Comment) (*comment.CommentDetail, error) {
+func (repo *CommentRepository) Update(comment *entity.Comment) (*comment.CommentDetailRow, error) {
 	result := repo.db.Model(&entity.Comment{}).
 	Where("id = ? AND author_id = ?", comment.ID, comment.AuthorID).
 	Updates(comment)
@@ -145,24 +147,24 @@ func (repo *CommentRepository) DeleteByID(commentID entity.CommentID, userID ent
 	return nil
 }
 
-func (repo *CommentRepository) FindByPostID(postID entity.PostID, userID entity.UserID) ([]comment.CommentList, error) {
-	var list []CommentListRow
+func (repo *CommentRepository) FindByPostID(postID entity.PostID, userID entity.UserID) ([]comment.CommentListRow, error) {
+	var comments []comment.CommentListRow
 
-	err := repo.db.Model(&entity.Comment{}).
+	if err := repo.db.Model(&entity.Comment{}).
 	Select(`
 		comments.id AS id,
 		comments.author_id AS author_id,
 		users.full_name AS full_name,
 		comments.post_id AS post_id,
-		comments.content AS content
-		comments.created_at AS created_at
+		comments.content AS content,
+		comments.created_at AS created_at,
 
 		(
 			SELECT COUNT(*)
 			FROM likes
 			WHERE likes.likeable_id = comments.id
 			AND likes.likeable_type = 'comment'
-		) AS likes
+		) AS likes,
 
 		EXISTS (
 			SELECT 1
@@ -170,7 +172,7 @@ func (repo *CommentRepository) FindByPostID(postID entity.PostID, userID entity.
 			WHERE likes.likeable_type = 'comment'
 			AND likes.likeable_id = comments.id
 			AND likes.user_id = ?
-		) AS liked
+		) AS liked,
 
 		(
 			SELECT COUNT(*)
@@ -181,16 +183,8 @@ func (repo *CommentRepository) FindByPostID(postID entity.PostID, userID entity.
 	`, userID).
 	Joins("JOIN users ON users.id = comments.author_id").
 	Where("post_id = ? AND parent_id IS NULL", postID).
-	Scan(&list).Error
-
-	if err != nil {
-		return nil, MapError(err)
-	}
-
-	comments := make([]comment.CommentList, len(list))
-
-	for i, comment := range list {
-		comments[i] = comment.ToCommentList()
+	Scan(&comments).Error; err != nil {
+		return nil, err
 	}
 
 	return comments, nil
