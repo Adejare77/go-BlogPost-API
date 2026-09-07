0eb9d9d8158bfd5ccc50e6da8735c5ef5fb59b48
Rashisky <rashisky007@gmail.com>
2026-09-07 15:11:21 +0100

    update post handler responses from service. update path_id logic

--- internal/delivery/http/handler/v1/post/handler.go
+++ internal/delivery/http/handler/v1/post/handler.go
@@ -6,18 +6,20 @@ import (
 
 	httperrors "github.com/Adejare77/go-BlogPost-API/internal/delivery/http/errors"
 	"github.com/Adejare77/go-BlogPost-API/internal/domain/entity"
-	"github.com/Adejare77/go-BlogPost-API/internal/domain/post"
-	"github.com/Adejare77/go-BlogPost-API/internal/usecase"
+	domainPost "github.com/Adejare77/go-BlogPost-API/internal/domain/post"
+	usecasePost "github.com/Adejare77/go-BlogPost-API/internal/usecase/post"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 )
 
 
 type PostHandler struct {
-	postService *usecase.PostService
+	postService *usecasePost.PostService
 }
 
-func NewPostHandler(postService *usecase.PostService) *PostHandler {
+func NewPostHandler(postService *usecasePost.PostService) *PostHandler {
 	return &PostHandler{
 		postService: postService,
 	}
@@ -46,43 +48,30 @@ func (h *PostHandler) Create(ctx *gin.Context) {
 
 	userID := ctx.MustGet("userID").(entity.UserID)
 
-	post := entity.Post{
+	postRequest := entity.Post{
 		AuthorID: userID,
 		Title: req.Title,
 		Content: req.Content,
 		IsPublished: req.IsPublished,
 	}
 
-	if err := h.postService.Create(&post); err != nil {
+	response, err := h.postService.Create(&postRequest)
+	if  err != nil {
 		httperrors.HandleError(ctx, err)
 		return
 	}
 
-	response := PostCreateResponse{
-		ID: post.ID,
-		Author: AuthorSummary{
-			ID: post.Author.ID,
-			FullName: post.Author.FullName,
-		},
-		Title: post.Title,
-		Content: post.Content,
-		IsPublished: post.IsPublished,
-		CreatedAt: post.CreatedAt,
-	}
-
-	ctx.JSON(http.StatusCreated, gin.H{
-		"results": response,
-	})
+	ctx.JSON(http.StatusCreated, response)
 }
 
 func (h *PostHandler) FindByID(ctx *gin.Context) {
-	var req PostPathRequest
+	var path PostPathRequest
 
-	if err := ctx.ShouldBindUri(&req); err != nil {
+	if err := ctx.ShouldBindUri(&path); err != nil {
 		var validationErrs validator.ValidationErrors
 
 		if errors.As(err, &validationErrs) {
-			httperrors.Validator(ctx, req, validationErrs)
+			httperrors.Validator(ctx, path, validationErrs)
 			return
 		}
 
@@ -93,9 +82,10 @@ func (h *PostHandler) FindByID(ctx *gin.Context) {
 			"invalid postId",
 			err,
 		)
+		return
 	}
 
-	postID := entity.PostID(req.PostID)
+	postID := entity.PostID(uuid.MustParse(path.PostID))
 
 	var userID entity.UserID
 	isStaff := false
@@ -106,45 +96,11 @@ func (h *PostHandler) FindByID(ctx *gin.Context) {
 		isStaff = ctx.MustGet("isStaff").(bool)
 	}
 
-	post, err := h.postService.FindByID(postID, userID, isStaff)
+	response, err := h.postService.FindByID(postID, userID, isStaff)
 
 	if err != nil {
 		httperrors.HandleError(ctx, err)
 		return
-	}
-
-	topComments := make([]CommentSummary, len(post.TopComments))
-
-	for i, c := range post.TopComments {
-		topComments[i] = CommentSummary{
-			ID: c.ID,
-			Author: AuthorSummary{
-				ID: c.Author.ID,
-				FullName: c.Author.FullName,
-			},
-			PostID: c.PostID,
-			Excerpt: c.Excerpt,
-			Likes: c.Likes,
-			Liked: c.Liked,
-			ReplyCount: c.ReplyCount,
-			CreatedAt: c.CreatedAt,
-		}
-	}
-
-	response := PostDetailResponse{
-		ID: post.ID,
-		Author: AuthorSummary{
-			ID: post.Author.ID,
-			FullName: post.Author.FullName,
-		},
-		Title: post.Title,
-		Content: post.Content,
-		Likes: post.Likes,
-		Liked: post.Liked,
-		IsPublished: post.IsPublished,
-		CommentCount: post.CommentCount,
-		TopComments: topComments,
-		CreatedAt: post.CreatedAt,
 	}
 
 	ctx.JSON(http.StatusOK, response)
@@ -192,7 +148,8 @@ func (h *PostHandler) Update(ctx *gin.Context) {
 		return
 	}
 
-	postID := entity.PostID(path.PostID)
+	postID := entity.PostID(uuid.MustParse(path.PostID))
+
 	userID := ctx.MustGet("userID").(entity.UserID)
 
 	post := entity.Post{
@@ -203,42 +160,10 @@ func (h *PostHandler) Update(ctx *gin.Context) {
 		IsPublished: req.IsPublished,
 	}
 
-	updatedPost, err := h.postService.Update(&post)
+	response, err := h.postService.Update(&post)
 	if err != nil {
 		httperrors.HandleError(ctx, err)
 		return
-	}
-
-	topComments := make([]CommentSummary, len(updatedPost.TopComments))
-
-	for i, c := range updatedPost.TopComments {
-		topComments[i] = CommentSummary{
-			ID: c.ID,
-			Author: AuthorSummary{
-				ID: c.Author.ID,
-				FullName: c.Author.FullName,
-			},
-			Excerpt: c.Excerpt,
-			Likes: c.Likes,
-			Liked: c.Liked,
-			CreatedAt: c.CreatedAt,
-
-		}
-	}
-
-	response := PostDetailResponse{
-		ID: updatedPost.ID,
-		Author: AuthorSummary{
-			ID: updatedPost.Author.ID,
-			FullName: updatedPost.Author.FullName,
-		},
-		Title: updatedPost.Title,
-		Content: updatedPost.Content,
-		Likes: updatedPost.Likes,
-		Liked: updatedPost.Liked,
-		IsPublished: updatedPost.IsPublished,
-		TopComments: topComments,
-		CreatedAt: updatedPost.CreatedAt,
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
@@ -247,13 +172,13 @@ func (h *PostHandler) Update(ctx *gin.Context) {
 }
 
 func (h *PostHandler) DeleteByID(ctx *gin.Context) {
-	var req PostPathRequest
+	var path PostPathRequest
 
-	if err := ctx.ShouldBindUri(&req); err != nil {
+	if err := ctx.ShouldBindUri(&path); err != nil {
 		var validationErrs validator.ValidationErrors
 
 		if errors.As(err, &validationErrs) {
-			httperrors.Validator(ctx, req, validationErrs)
+			httperrors.Validator(ctx, path, validationErrs)
 			return
 		}
 
@@ -267,7 +192,7 @@ func (h *PostHandler) DeleteByID(ctx *gin.Context) {
 		return
 	}
 
-	postID := entity.PostID(req.PostID)
+	postID := entity.PostID(uuid.MustParse(path.PostID))
 	userID := ctx.MustGet("userID").(entity.UserID)
 
 	if err := h.postService.DeleteByID(postID, userID); err != nil {
@@ -308,7 +233,7 @@ func (h *PostHandler) FindAll(ctx *gin.Context) {
 		isStaff = ctx.MustGet("isStaff").(bool)
 	}
 
-	query := post.PostQuery{
+	query := domainPost.PostQuery{
 		Status: reqQuery.Status,
 		Author: reqQuery.Author,
 	}
